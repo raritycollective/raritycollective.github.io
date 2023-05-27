6294f86880a709739592bbc9f3e4124fed8f609c
Andrew Michel <andrewmichel@bhb.local>
2023-05-27 14:42:54 -0700

    use hash router

--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,12 +1,12 @@
 import {
-  createBrowserRouter,
+  createHashRouter,
 } from "react-router-dom";
 import App from "../App";
 import { Appointments } from "../pages/appointments/Appointments";
 import { Home } from "../pages/home/Home";
 import { Contact } from "../pages/contact/Contact";
 
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: "/",
     element: <App />,
